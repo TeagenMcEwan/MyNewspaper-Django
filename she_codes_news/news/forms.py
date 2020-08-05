ed6dc4b020980697898d560509cf42eb4a0756c4
TeagenMcEwan <67173830+TeagenMcEwan@users.noreply.github.com>
2020-08-05 20:27:52 +0800

    Creating user accounts and disabling features

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -6,7 +6,7 @@ from .models import NewsStory
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ['title', 'author', 'pub_date', 'content', 'image']
+        fields = ['title', 'pub_date', 'content', 'image']
         widgets = {
             'pub_date': forms.DateInput(
                 format=('%m/%d/%Y'),
@@ -17,12 +17,6 @@ class StoryForm(ModelForm):
                 }
             ),
             'title': forms.TextInput(
-                attrs={
-                    'class':'form',
-                    'placeholder': 'Text Here'
-                }
-            ),
-             'author': forms.TextInput(
                 attrs={
                     'class':'form',
                     'placeholder': 'Text Here'
