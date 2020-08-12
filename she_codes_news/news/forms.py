5924f61f6ae435ffd2aa66ccaab701a6f19ac2f0
TeagenMcEwan <67173830+TeagenMcEwan@users.noreply.github.com>
2020-08-12 19:58:48 +0800

    Added update and delete story details

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -1,43 +1,53 @@
 from django import forms 
 from django.forms import ModelForm
 from .models import NewsStory
+# from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ['title', 'pub_date', 'content', 'image']
+        fields = ['title', 'pub_date', 'content', 'author', 'image']
         widgets = {
             'pub_date': forms.DateInput(
                 format=('%m/%d/%Y'),
                 attrs={
                     'class':'form',
                     'placeholder': 'Select a date',
-                    'type': 'date'
+                    'type': 'date',
                 }
             ),
             'title': forms.TextInput(
                 attrs={
                     'class':'form',
-                    'placeholder': 'Text Here'
+                    'placeholder': 'Text Here',
                 }
             ),
             'content': forms.TextInput(
                 attrs={
                     'class':'form',
-                    'placeholder': 'Text Here'
+                    'placeholder': 'Text Here',
                 }
             ),
+
+            'author': forms.TextInput(
+                attrs={
+                    'class': 'form',
+                    'placeholder': 'Text Here',
+                }
+            ),
+
             'image': forms.TextInput(
                 attrs={
                     'class':'form',
-                    'placeholder': 'Text Here'
+                    'placeholder': 'Insert URL',
                 }
             ),
             'button': forms.TextInput(
                 attrs={
                     'class':'form',
-                    'placeholder': 'Text Here'
+                    'placeholder': 'Text Here',
                 }
             ),
         }
